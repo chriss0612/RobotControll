01541816088a80f579d04a00fb0f44332820fbcb
chriss0612 <chrisi.schrefl@gmail.com>
2017-11-10 11:14:57 +0100

    added First code

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelOutL = new System.Windows.Forms.Label();
+            this.labelOutR = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.PortName = "COM6";
+            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelOutL
+            // 
+            this.labelOutL.AutoSize = true;
+            this.labelOutL.Location = new System.Drawing.Point(159, 280);
+            this.labelOutL.Name = "labelOutL";
+            this.labelOutL.Size = new System.Drawing.Size(13, 13);
+            this.labelOutL.TabIndex = 0;
+            this.labelOutL.Text = "L";
+            // 
+            // labelOutR
+            // 
+            this.labelOutR.AutoSize = true;
+            this.labelOutR.Location = new System.Drawing.Point(537, 280);
+            this.labelOutR.Name = "labelOutR";
+            this.labelOutR.Size = new System.Drawing.Size(15, 13);
+            this.labelOutR.TabIndex = 1;
+            this.labelOutR.Text = "R";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.labelOutR);
+            this.Controls.Add(this.labelOutL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelOutL;
+        private System.Windows.Forms.Label labelOutR;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int posx = 32;
+        private int posy = 32;
+        private bool posChanged = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                port.Open();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            posx = e.X;
+            posy = e.Y;
+            posChanged=true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if(posChanged)
+                {
+                    sendData(true, (byte)(posx * 64 / ClientSize.Width));
+                    sendData(false, (byte)(posy * 64 / ClientSize.Height));
+                    posChanged = false;
+                }
+            }
+            catch(DivideByZeroException)
+            {
+                
+            }
+        }
+        private void sendData(bool dir, int pos)
+        {
+            if(dir)
+            {
+                //Send Data for right motor
+                byte[] b = new byte[1];
+                b[0] = 0x80;                // 1000 0000
+                b[0] |= (byte)(0x3F & pos);
+                labelOutR.Text = "R: " + pos;
+                //port.Write(b, 0, 1);
+            }
+            else
+            {
+                //Send Data for left motor
+                byte[] b = new byte[1];
+                b[0] = 0x40;                // 0100 0000
+                b[0] |= (byte)(0x3F & pos);
+                labelOutL.Text = "L: " + pos;
+                //port.Write(b, 0, 1);
+            }
+        }
+    }
+}
