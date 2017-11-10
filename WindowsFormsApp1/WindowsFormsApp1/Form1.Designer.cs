3eff673a9c1d4ecb86139c2576edf9e63c473752
chriss0612 <chrisi.schrefl@gmail.com>
2017-11-10 11:58:27 +0100

    Theoraticly should be working maybe

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,51 +30,36 @@
         {
             this.components = new System.ComponentModel.Container();
             this.port = new System.IO.Ports.SerialPort(this.components);
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.labelOutL = new System.Windows.Forms.Label();
-            this.labelOutR = new System.Windows.Forms.Label();
+            this.speedBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
             this.SuspendLayout();
             // 
             // port
             // 
             this.port.PortName = "COM6";
-            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
             // 
-            // timer1
+            // speedBar
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 50;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // labelOutL
-            // 
-            this.labelOutL.AutoSize = true;
-            this.labelOutL.Location = new System.Drawing.Point(159, 280);
-            this.labelOutL.Name = "labelOutL";
-            this.labelOutL.Size = new System.Drawing.Size(13, 13);
-            this.labelOutL.TabIndex = 0;
-            this.labelOutL.Text = "L";
-            // 
-            // labelOutR
-            // 
-            this.labelOutR.AutoSize = true;
-            this.labelOutR.Location = new System.Drawing.Point(537, 280);
-            this.labelOutR.Name = "labelOutR";
-            this.labelOutR.Size = new System.Drawing.Size(15, 13);
-            this.labelOutR.TabIndex = 1;
-            this.labelOutR.Text = "R";
+            this.speedBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.speedBar.Location = new System.Drawing.Point(0, 0);
+            this.speedBar.Maximum = 31;
+            this.speedBar.Name = "speedBar";
+            this.speedBar.Size = new System.Drawing.Size(420, 49);
+            this.speedBar.TabIndex = 0;
+            this.speedBar.Value = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 661);
-            this.Controls.Add(this.labelOutR);
-            this.Controls.Add(this.labelOutL);
+            this.ClientSize = new System.Drawing.Size(420, 49);
+            this.Controls.Add(this.speedBar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,9 +68,7 @@
         #endregion
 
         private System.IO.Ports.SerialPort port;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label labelOutL;
-        private System.Windows.Forms.Label labelOutR;
+        private System.Windows.Forms.TrackBar speedBar;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,8 +12,11 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private int posx = 32;
-        private int posy = 32;
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+
         private bool posChanged = false;
         public Form1()
         {
@@ -36,31 +39,76 @@ namespace WindowsFormsApp1
                 MessageBox.Show(E.Message);
             }
         }
-
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void Drive(bool LRPress, bool LR, bool FBPress, bool FB)
         {
-            posx = e.X;
-            posy = e.Y;
-            posChanged=true;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            try
+            if(LRPress)
             {
-                if(posChanged)
+                if(FBPress)
                 {
-                    sendData(true, (byte)(posx * 64 / ClientSize.Width));
-                    sendData(false, (byte)(posy * 64 / ClientSize.Height));
-                    posChanged = false;
+                    if(FB)
+                    {
+                        if(LR)
+                        {
+                            SendData(true, 32 - speedBar.Value / 2);       //Forward Left
+                            SendData(false, 32 + speedBar.Value);
+                        }
+                        else
+                        {
+                            SendData(true, 32 - speedBar.Value);       //Forward Right
+                            SendData(false, 32 + speedBar.Value / 2);
+                        }
+                    }
+                    else
+                    {
+                        if (LR)
+                        {
+                            SendData(true, 32 + speedBar.Value / 2);       //Backward Left
+                            SendData(false, 32 - speedBar.Value);
+                        }
+                        else
+                        {
+                            SendData(true, 32 + speedBar.Value);       //Backward Right
+                            SendData(false, 32 - speedBar.Value / 2);
+                        }
+                    }
+                }
+                else
+                {
+                    if(LR)
+                    {
+                        SendData(true, 32 + speedBar.Value);           //Turn Left
+                        SendData(false, 32 + speedBar.Value);
+                    }
+                    else
+                    {
+                        SendData(true, 32 - speedBar.Value);           //Turn Right
+                        SendData(false, 32 - speedBar.Value);
+                    }
                 }
             }
-            catch(DivideByZeroException)
+            else
             {
-                
+                if(FBPress)
+                {
+                    if(FB)
+                    {
+                        SendData(true, 32 - speedBar.Value);       //Forward Straight
+                        SendData(false, 32 + speedBar.Value);
+                    }
+                    else
+                    {
+                        SendData(true, 32 + speedBar.Value);       //Backward Straight
+                        SendData(false, 32 - speedBar.Value);
+                    }
+                }
+                else
+                {
+                    SendData(true, 32);         //Stop
+                    SendData(false, 32);
+                }
             }
         }
-        private void sendData(bool dir, int pos)
+        private void SendData(bool dir, int pos)
         {
             if(dir)
             {
@@ -68,8 +116,7 @@ namespace WindowsFormsApp1
                 byte[] b = new byte[1];
                 b[0] = 0x80;                // 1000 0000
                 b[0] |= (byte)(0x3F & pos);
-                labelOutR.Text = "R: " + pos;
-                //port.Write(b, 0, 1);
+                port.Write(b, 0, 1);
             }
             else
             {
@@ -77,9 +124,54 @@ namespace WindowsFormsApp1
                 byte[] b = new byte[1];
                 b[0] = 0x40;                // 0100 0000
                 b[0] |= (byte)(0x3F & pos);
-                labelOutL.Text = "L: " + pos;
-                //port.Write(b, 0, 1);
+                port.Write(b, 0, 1);
             }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                down = false;
+                up = true;
+            }
+            else if(e.KeyCode == Keys.S)
+            {
+                up = false;
+                down = true;
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                right = false;
+                left = true;
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                left = false;
+                right = true;
+            }
+            Drive(left || right, left, up || down, up);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                up = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                down = false;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+            Drive(left || right, left, up || down, up);
         }
     }
 }
